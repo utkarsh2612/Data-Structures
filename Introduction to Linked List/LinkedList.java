e0b8f151d5c9223abaada5552ab60b7f781195ae
utkarsh2612 <utkarshchadhry26@gmail.com>
2016-04-03 01:22:29 +0530

    Update LinkedList.java

--- Introduction to Linked List/LinkedList.java
+++ Introduction to Linked List/LinkedList.java
@@ -1,10 +1,10 @@
 class LinkedList
 {
-	static class Node
+	static class Node		//this is class Node which provide basic structure for linked list i.e. node
 	{
 		int data;
 		Node next;
-		Node(int d)
+		Node(int d)		//here data and next to Node is initialised
 		{
 			data=d;
 			next=null;
@@ -15,7 +15,7 @@ class LinkedList
 	
 	public void printList()
 	{
-		Node n = head;
+		Node n = head;		//n is just like temp variable that is used to traverse complete list and print data
 		while(n!=null)
 		{
 			System.out.println(n.data+" ");
@@ -25,13 +25,13 @@ class LinkedList
 
 	public static void main(String args[])
 	{
-		LinkedList llist=new LinkedList();
-		llist.head = new Node(1);
+		LinkedList llist=new LinkedList();	//we created an object of class LinkedList i.e llist
+		llist.head = new Node(1);		//with llist we used head node and called it constructor with value 1
 		Node first = new Node(2);
 		Node second = new Node(3);
 		
-		llist.head.next=first;
+		llist.head.next=first;			//here we are linking various nodes of llist so that they could know which to point next
 		first.next=second;
-		llist.printList();
+		llist.printList();		//simply printing the list
 	}
 }
